6b0d8769ec773f06169b0be1945487d06ad85a93
Dominic Quentin <dominic.quentin@gmail.com>
2018-04-06 22:20:16 +0200

    header redesign

--- frontend/app/containers/App/index.js
+++ frontend/app/containers/App/index.js
@@ -14,12 +14,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
+import Header from 'components/Header';
+import HomePage from 'containers/HomePage/Loadable'; 
+
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
 export default function App() {
   return (
     <div>
+    <Header />
+
+
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route component={NotFoundPage} />
