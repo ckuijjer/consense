7e31c15c043db89fbf4accd6427f48220dc72ebd
Dominic Quentin <dominic.quentin@gmail.com>
2018-04-07 14:27:01 +0200

    remove boilerplate

---
+++ frontend/client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Photo from './Photo';
+
+
+const PhotoGrid = React.createClass({
+	render() {
+		return (
+			<p>this is the grid view</p>
+		)
+	}
+});
+
+export default PhotoGrid;
