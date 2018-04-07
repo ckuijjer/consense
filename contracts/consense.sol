bc15644da8071caa8933951b929e779eb5df964d
maciej-acer <arispen@gmail.com>
2018-04-07 12:48:53 +0200

    contract version 0.2.0

--- contracts/consense.sol
+++ contracts/consense.sol
@@ -1,35 +1,43 @@
 pragma solidity ^0.4.21;
 
 /**
- * Consense contract version 0.1.0
+ * Consense contract version 0.2.0
  */
 
 contract Consense {
 
-    address creator;
-
     struct Consent {
+        /*
         string metaDataHash;
         string preferencesHash;
         string matchmakingHash;
+        */
         bool isParticipating;
         bool consentGiven;
     }
 
     mapping(address => Consent) public consents;
 
-    function Consense(address[] _participantList) public {
-        for (uint index = 0; index < _participantList.length; index++) {
-            consents[_participantList[index]].isParticipating = true;
-        }
-        creator = msg.sender;
+    modifier onlyParticipant(){
+        require(consents[msg.sender].isParticipating == true);
+        _;
     }
 
-    function giveConsent() public {
+    function Consense(address[] _participantList) public {
+        for (uint i = 0; i < _participantList.length; i++) {
+            consents[_participantList[i]].isParticipating = true;
+        }
+    }
+
+    function giveConsent() 
+        public 
+        onlyParticipant {
         consents[msg.sender].consentGiven = true;
     }
 
-    function withdrawConsent() public {
+    function withdrawConsent() 
+        public 
+        onlyParticipant {
         consents[msg.sender].consentGiven = false;
     }
 
